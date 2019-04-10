d508c51350e404a2b540f9d039b9f8638c3bfc39
Arslan Ali <ali.arslan319@gmail.com>
2019-04-10 22:04:01 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -10,20 +10,20 @@ end
   puts "You passed the quiz!"
 
 
-  1.	def greeting
-  2.	    puts "Please type your name:"
-  3.	    name = gets
-  4.	    puts "Your name is #{name}!"
-  5.	end
-  6.
-  7.	greeting
-
-
-  def annoying
-  	loop do
-  		puts "What's up, Doc?"
-  		answer = gets.chomp
-  		break if answer == "STOP"
-  	end
-  	puts "Okay, okay, jeez. I'll stop. Sorry."
-  end
+  # 1.	def greeting
+  # 2.	    puts "Please type your name:"
+  # 3.	    name = gets
+  # 4.	    puts "Your name is #{name}!"
+  # 5.	end
+  # 6.
+  # 7.	greeting
+  #
+  #
+  # def annoying
+  # 	loop do
+  # 		puts "What's up, Doc?"
+  # 		answer = gets.chomp
+  # 		break if answer == "STOP"
+  # 	end
+  # 	puts "Okay, okay, jeez. I'll stop. Sorry."
+  # end
