2d414fe8c1709c7c3442be2b96f726a873dd9506
Arslan Ali <ali.arslan319@gmail.com>
2019-04-10 22:03:45 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,8 +1,29 @@
 
 def levitation_quiz
-  puts "What is the spell that enacts levitation?"
+  loop do
+      puts "What is the spell that enacts levitation?"
 answer = gets.chomp
   break if == "Wingardium Leviosa"
+    end
 end
 
   puts "You passed the quiz!"
+
+
+  1.	def greeting
+  2.	    puts "Please type your name:"
+  3.	    name = gets
+  4.	    puts "Your name is #{name}!"
+  5.	end
+  6.	 
+  7.	greeting
+
+
+  def annoying
+  	loop do 
+  		puts "What's up, Doc?"
+  		answer = gets.chomp
+  		break if answer == "STOP"
+  	end
+  	puts "Okay, okay, jeez. I'll stop. Sorry."
+  end
